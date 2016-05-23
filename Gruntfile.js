60df9254da3442f717c06c3e891a5c45fd32c222
Bart Libert <bart.libert@gmail.com>
2016-05-23 16:18:28 +0200

    Modify grunt steerfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,19 @@ module.exports = function(grunt) {
 				banner: '/*!\n * jsTodoTxt v<%= pkg.version %>\n * Built on <%= grunt.template.today("yyyy-mm-dd") %>\n * https://github.com/jmhobbs/jsTodoTxt\n *\n * Copyright 2011-<%= grunt.template.today("yyyy") %>, John Hobbs\n * Licensed under the MIT license.\n * https://github.com/jmhobbs/jsTodoTxt/blob/master/LICENSE\n */'
 			},
 			build: {
-				src: "jsTodoTxt.js",
+				src: [
+					"jsTodoTxt.js",
+					"jsTodoExtensions"
+				],
 				dest: "jsTodoTxt.min.js"
 			}
 		},
 		jasmine: {
 			jsTodoTxt: {
-				src: "jsTodoTxt.js",
+				src: [
+					"jsTodoTxt.js",
+					"jsTodoExtensions.js"
+				],
 				options: {
 					outfile: "test/SpecRunner.html",
 					keepRunner: true,
