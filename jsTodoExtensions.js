4192ba826a92f6313e7ddee1df6000f2c2988035
Davide Ferrero <info@davideferrero.com>
2018-10-02 17:21:03 +0200

    Fixed indentation.

--- jsTodoExtensions.js
+++ jsTodoExtensions.js
@@ -1,22 +1,22 @@
 /*!
-    Extensions to the todo.txt format
+	Extensions to the todo.txt format
 */
 
 function TodoTxtExtension( name ) {
-    this.reset = function () {
-        this.name = null;
-        this.parsingFunction = null;
-    };
+	this.reset = function () {
+		this.name = null;
+		this.parsingFunction = null;
+	};
     // The parsing function should return an array containing
     // the real value of the element, the parsed task line and
     // the string representation of the value.
-    this.parsingFunction = function ( line ) {
-        return [null, null, null];
-    };
+	this.parsingFunction = function ( line ) {
+		return [null, null, null];
+	};
 }
 
 function HiddenExtension() {
-    this.name = "hidden";
+	this.name = "hidden";
 }
 
 HiddenExtension.prototype = new TodoTxtExtension();
@@ -26,7 +26,7 @@ HiddenExtension.prototype.parsingFunction = function(line) {
     if ( matchHidden !== null ) {
         hidden = true;
     }
-    return [hidden, line.replace(/h:1/, ''), null];
+	return [hidden, line.replace(/h:1/, ''), null];
 };
 
 function DueExtension() {
