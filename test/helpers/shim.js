e0550748d7d0ae20943ab2c6969ccce6a263aeec
John Hobbs <john@velvetcache.org>
2018-10-03 10:13:54 -0500

    Update build and test tooling.

---
+++ test/helpers/shim.js
@@ -0,0 +1,8 @@
+// Shim things onto the global object.
+global.TodoTxtItem = require("../../jsTodoTxt.js").TodoTxtItem;
+global.TodoTxtItemHelper = require("../../jsTodoTxt.js").TodoTxtItemHelper;
+
+// And extensions...
+global.TodoTxtExtension = require("../../jsTodoExtensions.js").TodoTxtExtension;
+global.HiddenExtension = require("../../jsTodoExtensions.js").HiddenExtension;
+global.DueExtension = require("../../jsTodoExtensions.js").DueExtension;
