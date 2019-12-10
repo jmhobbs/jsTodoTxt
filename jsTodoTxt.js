e7ad58832d96ba15f0667aa5aa704dc8176ccf17
Kirill Goncharov <kdgoncharov@gmail.com>
2019-12-11 00:03:31 +0300

    Allow empty tasks

--- jsTodoTxt.js
+++ jsTodoTxt.js
@@ -149,8 +149,6 @@ function TodoTxtItem ( line, extensions ) {
 		Parse a string into this object.
 
 		\param line A string in todo.txt format to parse.
-
-		\throws Exception On an empty task.
 	*/
 	this.parse = function ( line ) {
 		var date_pieces;
@@ -232,9 +230,6 @@ function TodoTxtItem ( line, extensions ) {
 		line = line.replace( TodoTxt._trim_re, '');
 
 		this.text = line;
-
-		// If we have an empty task, not much point in creating an object.
-		if( "" === this.text ) { throw new Exception( "Empty Task" ); }
 	};
 
 	this._getNumOfNulls = function( lhs, rhs ) {
