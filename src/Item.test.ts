7aa92f1a17bd15b18650e3264d7f19143873c930
John Hobbs <john@velvetcache.org>
2022-10-19 12:07:55 -0500

    Flesh out Item API

--- src/Item.test.ts
+++ src/Item.test.ts
@@ -3,9 +3,11 @@ import test from 'ava'
 import Item from './Item'
 
 interface Extension {
-	tag: string
+	key: string
 	value: string
 }
+
+// constructor
 
 function constructAndCompare(
 	t: any,
@@ -49,14 +51,241 @@ test(
 	'A',
 	new Date(2016, 0, 2),
 	'measure space for +chapelShelving @chapel due:2016-05-03',
-	['chapelShelving'],
 	['chapel'],
-	[{tag: 'due', value: '2016-05-03'}]
+	['chapelShelving'],
+	[{key: 'due', value: '2016-05-03'}]
 );
 
+// Outputs
+
+const sampleCompleted = 'x (Z) 2022-10-17 We should keep +todoItems in their @place when rendering out due:2022-10-22';
 
 test('toString › Keeps positioning of tags', t => {
-	const itemString = 'x (Z) 2022-10-17 We should keep +todoItems in their @place when rendering out';
-	const item = new Item(itemString);
-	t.is(item.toString(), itemString);
+	const item = new Item(sampleCompleted);
+	t.is(item.toString(), sampleCompleted);
+});
+
+test('toString › Appends new tags to the end', t => {
+	const item = new Item(sampleCompleted);
+	item.addProject('rewrite');
+	item.addContext('computer');
+	item.addExtension('h', '1');
+	t.is(item.toString(), `${sampleCompleted} +rewrite @computer h:1`);
+})
+
+test('toAnnotatedString › Returns the correct string', t => {
+	const itemStr = '(B) 2022-01-04 My @wall is +painted the color:blue';
+	const item = new Item(itemStr);
+	const annotated = item.toAnnotatedString();
+	t.is(annotated.string, itemStr);
+});
+
+test('toAnnotatedString › Returns the correct ranges', t => {
+	const itemStr = '(B) 2022-01-04 My @wall is +painted the color:blue';
+	const item = new Item(itemStr);
+	const annotated = item.toAnnotatedString();
+	t.deepEqual(annotated.contexts, [{
+		tag: 'wall',
+		start: 18,
+		end: 23,
+	}]);
+	t.deepEqual(annotated.projects, [{
+		tag: 'painted',
+		start: 27,
+		end: 35,
+	}]);
+	t.deepEqual(annotated.extensions, [{
+		key: 'color',
+		value: 'blue',
+		start: 40,
+		end: 51,
+	}]);
+	t.is(annotated.string.slice(18, 23), '@wall');
+	t.is(annotated.string.slice(27, 35), '+painted');
+	t.is(annotated.string.slice(40, 51), 'color:blue');
+});
+
+// Header
+
+test('setCompleted › Works marking complete', t => {
+	const item = new Item('I have to do this.');
+	t.false(item.completed());
+	item.setCompleted(true);
+	t.true(item.completed());
+	t.is(item.toString(), 'x I have to do this.');
+});
+
+test('setCompleted › Works marking incomplete', t => {
+	const item = new Item('x I have to do this.');
+	t.true(item.completed());
+	item.setCompleted(false);
+	t.false(item.completed());
+	t.is(item.toString(), 'I have to do this.');
+});
+
+test('setPriority › Adding', t => {
+	const item = new Item('I have to do this.');
+	item.setPriority('T');
+	t.is(item.priority(), 'T');
+	t.is(item.toString(), '(T) I have to do this.');
+});
+
+test('setPriority › Updating', t => {
+	const item = new Item('(Z) I have to do this.');
+	item.setPriority('T');
+	t.is(item.priority(), 'T');
+	t.is(item.toString(), '(T) I have to do this.');
+});
+
+test('setPriority › Removing', t => {
+	const item = new Item('(L) I have to do this.');
+	item.setPriority();
+	t.is(item.priority(), null);
+	t.is(item.toString(), 'I have to do this.');
+});
+
+test('setDueDate › Adding with Date', t => {
+	const item = new Item('I have to do this.');
+	const due = new Date(2022, 7, 1);
+	item.setDate(due);
+	t.deepEqual(item.date(), due);
+});
+
+test('setDueDate › Adding with string', t => {
+	const item = new Item('I have to do this.');
+	const due = new Date(2022, 6, 1);
+	item.setDate('2022-07-01');
+	t.deepEqual(item.date(), due);
+});
+
+test('setDueDate › Updating with Date', t => {
+	const item = new Item('1999-04-12 I have to do this.');
+	const due = new Date(2022, 7, 1);
+	item.setDate(due);
+	t.deepEqual(item.date(), due);
+});
+
+test('setDueDate › Updating with string', t => {
+	const item = new Item('1999-04-12 I have to do this.');
+	item.setDate('2022-07-01');
+	t.deepEqual(item.date(), new Date(2022, 6, 1));
+});
+
+test('setDueDate › Removing', t => {
+	const item = new Item('1999-04-12 I have to do this.');
+	item.setDate();
+	t.is(item.date(), null);
+});
+
+// Context
+
+test('addContext › Adds new contexts', t => {
+	const item = new Item(sampleCompleted);
+	item.addContext('computer');
+	t.deepEqual(item.contexts(), ['place', 'computer'])
+});
+
+test('addContext › Does not add projects which already exist', t => {
+	const item = new Item(sampleCompleted);
+	item.addContext('place');
+	t.deepEqual(item.contexts(), ['place']);
+});
+
+test('addContext › Updates the body', t => {
+	const item = new Item('Hello');
+	item.addContext('world');
+	t.is(item.body(), 'Hello @world');
+});
+
+test('removeContext › Removes contexts', t => {
+	const item = new Item('Hello @home and @work with +projects and @work extensions:todo');
+	item.removeContext('work');
+	t.deepEqual(item.contexts(), ['home']);
+});
+
+test('removeContext › Updates the body', t => {
+	const item = new Item('Hello @home and @work with +projects and @work extensions:todo');
+	item.removeContext('work');
+	t.is(item.body(), 'Hello @home and with +projects and extensions:todo');
+});
+
+// Project
+
+test('addProject › Adds new projects', t => {
+	const item = new Item(sampleCompleted);
+	item.addProject('rewrite');
+	t.deepEqual(item.projects(), ['todoItems', 'rewrite'])
+});
+
+test('addProject › Does not add projects which already exist', t => {
+	const item = new Item(sampleCompleted);
+	item.addProject('todoItems');
+	t.deepEqual(item.projects(), ['todoItems']);
+});
+
+test('addProject › Updates the body', t => {
+	const item = new Item('Hello');
+	item.addProject('world');
+	t.is(item.body(), 'Hello +world');
+});
+
+test('removeProject › Removes projects', t => {
+	const item = new Item('Hello @home with +goals and +projects and +goals extensions:todo');
+	item.removeProject('goals');
+	t.deepEqual(item.projects(), ['projects']);
+});
+
+test('removeProject › Updates the body', t => {
+	const item = new Item('Hello @home with +goals and +projects and +goals extensions:todo');
+	item.removeProject('goals');
+	t.is(item.body(), 'Hello @home with and +projects and extensions:todo')
+});
+
+// Extensions
+
+test('setExtension › Overwrites existing values', t => {
+	const item = new Item('Party like its due:2022-10-22');
+	item.setExtension('due', '1999-12-31')
+	t.deepEqual(item.extensions(), [{key: 'due', value: '1999-12-31'}]);
+	t.is(item.body(), 'Party like its due:1999-12-31')
+});
+
+test('setExtension › Removes additional values', t => {
+	const item = new Item('My wall is painted the color:blue color:yellow @home for +housePainting');
+	item.setExtension('color', 'red')
+	t.deepEqual(item.extensions(), [{key: 'color', value: 'red'}]);
+	t.is(item.body(), 'My wall is painted the color:red @home for +housePainting')
+});
+
+test('addExtension › Allows for multiple of the same key', t => {
+	const item = new Item('My wall is painted the color:blue');
+	item.addExtension('color', 'red')
+	t.deepEqual(item.extensions(), [{key: 'color', value:'blue'}, {key: 'color', value: 'red'}]);
+	t.is(item.body(), 'My wall is painted the color:blue color:red')
+});
+
+test('removeExtension › Removes the extension by key', t => {
+	const item = new Item('My room:kitchen wall is painted color:blue and color:red');
+	item.removeExtension('color');
+	t.deepEqual(item.extensions(), [{key: 'room', value: 'kitchen'}]);
+	t.is(item.body(), 'My room:kitchen wall is painted and')
+});
+
+test('removeExtension › Removes the extension by key and value', t => {
+	const item = new Item('My room:kitchen wall is painted color:blue and color:red');
+	item.removeExtension('color', 'blue');
+	t.deepEqual(item.extensions(), [{key: 'room', value: 'kitchen'}, {key: 'color', value: 'red'}]);
+	t.is(item.body(), 'My room:kitchen wall is painted and color:red')
+});
+
+// Body
+
+test('setBody › Updates contexts, projects and extensions', t => {
+ const item = new Item('This is @before and +willDelete these tags:all');
+ const newBody = 'A new @world with +newTags and extension:values';
+ item.setBody(newBody);
+ t.deepEqual(item.contexts(), ['world']);
+ t.deepEqual(item.projects(), ['newTags']);
+ t.deepEqual(item.extensions(), [{key: 'extension', value: 'values'}]);
+ t.is(item.body(), newBody);
 });
--- src/Item.ts
+++ src/Item.ts
@@ -1,32 +1,75 @@
 const rTodo = /^((x) )?(\(([A-Z])\) )?((\d{4}-\d{2}-\d{2}) )?(.*)$/;
 const rTags = /([^\s:]+:[^\s:]+|[+@]\S+)/g;
 
-interface Context {
+interface Tag {
 	tag: string
-	offset: number
-}
-
-interface Project {
-	tag: string
-	offset: number
+	start: number
+	end?: number
 }
 
 interface Extension {
-	tag: string
+	key: string
 	value: string
-	offset: number
+	start: number
+	end?: number
 }
 
+type Context = Tag;
+type Project = Tag;
+
 type Priority = string | null;
+
+interface AnnotatedItem {
+	string: string
+	// todo: date, priority, etc
+	contexts: Context[]
+	projects: Project[]
+	extensions: Extension[]
+}
+
+function parseBody(body: string) {
+	let start = 0;
+	const tags = (body.match(rTags) || []).map((tag):[string, number] => {
+		const tagStart = body.indexOf(tag, start);
+		if(tagStart != -1) { start = tagStart + tag.length; }
+		return [tag, tagStart];
+	});
+
+
+	const contexts: Context[] = [];
+	const projects: Project[] = [];
+	const extensions: Extension[] = [];
+
+	tags.forEach(([tag, start]) => {
+		if(tag[0] == '@') {
+			contexts.push({ tag: tag.slice(1), start });
+		} else if (tag[0] == '+'){
+			projects.push({ tag: tag.slice(1), start });
+		} else {
+			const split = tag.split(':', 2)
+			extensions.push({key: split[0], value: split[1], start});
+		}
+	});
+
+	return { contexts, projects, extensions }
+}
+
+function dateFromString(input: string):Date {
+	return new Date(
+		parseInt(input.slice(0, 4), 10),
+		parseInt(input.slice(5, 7), 10) - 1, // months are zero indexed
+		parseInt(input.slice(8), 10)
+	);
+}
 
 export default class Item {
 	#complete: boolean = false;
 	#priority: Priority = null;
 	#date: Date | null = null;
 	#body: string = '';
-	#contexts = new Array<Context>();
-	#projects = new Array<Project>();;
-	#extensions = new Array<Extension>();
+	#contexts: Context[] = [];
+	#projects: Project[] = [];
+	#extensions: Extension[] = [];
 
 	constructor(line: string) {
 		const match = rTodo.exec(line);
@@ -37,31 +80,9 @@ export default class Item {
 		this.#complete = match[2] === 'x'
 		this.#priority = match[4] || null;
 		if(typeof match[6] !== 'undefined') {
-			this.#date = new Date(
-				parseInt(match[6].slice(0, 4), 10),
-				parseInt(match[6].slice(5, 7), 10) - 1, // months are zero indexed
-				parseInt(match[6].slice(8), 10)
-			);
+			this.#date = dateFromString(match[6]);
 		}
-		this.#body = match[7];
-
-		let offset = 0;
-		const tags = (this.#body.match(rTags) || []).map((tag):[string, number] => {
-			const tagOffset = this.#body.indexOf(tag, offset);
-			if(tagOffset != -1) { offset = tagOffset + tag.length; }
-			return [tag, tagOffset];
-		});
-
-		tags.forEach(([tag, offset]) => {
-			if(tag[0] == '+') {
-				this.#contexts.push({ tag: tag.slice(1), offset });
-			} else if (tag[0] == '@'){
-				this.#projects.push({ tag: tag.slice(1), offset });
-			} else {
-				const split = tag.split(':', 2)
-				this.#extensions.push({tag: split[0], value: split[1], offset});
-			}
-		});
+		this.setBody(match[7]);
 	}
 
 	toString() {
@@ -75,12 +96,50 @@ export default class Item {
 		return parts.filter((v) => v !== null && v !== '').join(' ');
 	}
 
+	toAnnotatedString():AnnotatedItem {
+		const str = this.toString()
+		const headerLength = str.length - this.#body.length;
+
+		function tagRemap (tag:Tag):Tag {
+			return {
+				tag: tag.tag,
+				start: tag.start + headerLength,
+				end: tag.start + headerLength + tag.tag.length + 1
+			};
+		};
+
+		function extensionsRemap (ext:Extension):Extension {
+			return {
+				key: ext.key,
+				value: ext.value,
+				start: ext.start + headerLength,
+				end: ext.start + headerLength + `${ext.key}:${ext.value}`.length + 1
+			};
+		};
+
+		return {
+			string: str,
+			contexts: this.#contexts.map(tagRemap),
+			projects: this.#projects.map(tagRemap),
+			extensions: this.#extensions.map(extensionsRemap)
+		}
+	}
+
 	completed() {
 		return this.#complete;
 	}
 
+	setCompleted(completed: boolean) {
+		this.#complete = completed;
+	}
+
 	priority() {
 		return this.#priority;
+	}
+
+	setPriority(priority:string|null=null) {
+		// todo: validate priority
+		this.#priority = priority;
 	}
 
 	date() {
@@ -96,19 +155,161 @@ export default class Item {
 		return '';
 	};
 
+	setDate(date: Date|string|null=null) {
+		if(date === null || date instanceof Date) {
+			this.#date = <Date|null> date;
+		} else {
+			// todo: validate date string
+			this.#date = dateFromString(<string> date);
+		}
+	}
+
 	body() {
 		return this.#body;
+	}
+
+	setBody(body:string) {
+		const { contexts, projects, extensions } = parseBody(body);
+		this.#body = body;
+		this.#contexts = contexts;
+		this.#projects = projects;
+		this.#extensions = extensions;
 	}
 
 	contexts() {
 		return this.#contexts.map(({tag}) => tag);
 	}
 
+	addContext(tag: string) {
+		if(this.#contexts.filter((v) => tag === v.tag).length > 0) {
+			return
+		}
+		this.#contexts.push({tag, start: this.#body.length});
+		this.#body = [this.#body, `@${tag}`].join(' ');
+	}
+
+	removeContext(tag: string) {
+		const body = removeTag(this.#body, this.#contexts, tag);
+		if(body !== null) {
+			this.#body = body;
+
+			const { contexts, projects, extensions } = parseBody(this.#body);
+			this.#contexts = contexts;
+			this.#projects = projects;
+			this.#extensions = extensions;
+		}
+	}
+
 	projects() {
 		return this.#projects.map(({tag}) => tag);
 	}
 
-	extensions() {
-		return this.#extensions.map(({tag, value}) => { return {tag, value} });
+	addProject(tag: string) {
+		if(this.#projects.filter((v) => tag === v.tag).length > 0) {
+			return
+		}
+		this.#projects.push({tag, start: this.#body.length});
+		this.#body = [this.#body, `+${tag}`].join(' ');
 	}
+
+	removeProject(tag: string) {
+		const body = removeTag(this.#body, this.#projects, tag);
+		if(body !== null) {
+			this.#body = body;
+
+			const { contexts, projects, extensions } = parseBody(this.#body);
+			this.#contexts = contexts;
+			this.#projects = projects;
+			this.#extensions = extensions;
+		}
+	}
+
+	extensions() {
+		return this.#extensions.map(({key, value}) => { return {key, value} });
+	}
+
+	setExtension(key: string, value: string) {
+		let found: boolean = false;
+
+		this.#extensions.forEach(ext => {
+			if(ext.key === key) {
+				const prefix = this.#body.slice(0, ext.start);
+				const suffix = this.#body.slice(ext.start + ext.key.length + ext.value.length + 1);
+				if(found) {
+					this.#body = [
+						prefix.slice(0, prefix.length - 1), // take the extra space off the end of prefix
+						suffix
+					].join('');
+				} else {
+					this.#body = [
+						prefix,
+						`${key}:${value}`,
+						suffix
+					].join('');
+				}
+				found = true;
+			}
+		});
+
+		if(found) {
+			const { contexts, projects, extensions } = parseBody(this.#body);
+			this.#contexts = contexts;
+			this.#projects = projects;
+			this.#extensions = extensions;
+		} else {
+			this.addExtension(key, value);
+		}
+	}
+
+	addExtension(key: string, value: string) {
+		this.#extensions.push({key, value, start: this.#body.length});
+		this.#body = [this.#body, `${key}:${value}`].join(' ');
+	}
+
+	removeExtension(key: string, value: string|null=null) {
+		const spans = this.#extensions
+			.filter(ext => {
+				return ext.key === key && (value === null || ext.value === value)
+			})
+			.map(ext => { return {start: ext.start, end: ext.start + ext.key.length + ext.value.length + 1} })
+			.sort((a, b) => (a.start < b.start) ? 1 : -1);
+
+		if(spans.length > 0) {
+			this.#body = cutOutSpans(this.#body, spans);
+
+			const { contexts, projects, extensions } = parseBody(this.#body);
+			this.#contexts = contexts;
+			this.#projects = projects;
+			this.#extensions = extensions;
+		}
+	}
+}
+
+interface Span {
+	start: number
+	end: number
+}
+
+function cutOutSpans(body: string, spans: Span[]):string {
+	spans.forEach(({start, end}) => {
+		body = [
+			body.slice(0, start - 1),
+			body.slice(end)
+		].join('');
+	})
+
+	return body;
+}
+
+function removeTag(body: string, tags: Tag[], tag: string):string|null {
+	const spans = tags
+		.filter(ctx => ctx.tag === tag)
+		.map(ctx => { return {start: ctx.start, end: ctx.start + ctx.tag.length + 1} })
+		.sort((a, b) => (a.start < b.start) ? 1 : -1);
+
+	if(spans.length === 0) {
+		return null;
+	}
+
+	return cutOutSpans(body, spans);
 }
