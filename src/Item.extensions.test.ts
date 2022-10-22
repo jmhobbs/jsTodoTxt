7deb76051783b723a30513c34cce1fa0ad2793e7
John Hobbs <john@velvetcache.org>
2022-10-21 21:56:18 -0500

    Fix lints

--- src/Item.extensions.test.ts
+++ src/Item.extensions.test.ts
@@ -1,37 +1,43 @@
-import test from 'ava'
-import { Item } from './Item'
+import test from 'ava';
+import { Item } from './Item';
 
-test('setExtension › Overwrites existing values', t => {
+test('setExtension › Overwrites existing values', (t) => {
 	const item = new Item('Party like its due:2022-10-22');
-	item.setExtension('due', '1999-12-31')
-	t.deepEqual(item.extensions(), [{key: 'due', value: '1999-12-31'}]);
-	t.is(item.body(), 'Party like its due:1999-12-31')
+	item.setExtension('due', '1999-12-31');
+	t.deepEqual(item.extensions(), [{ key: 'due', value: '1999-12-31' }]);
+	t.is(item.body(), 'Party like its due:1999-12-31');
 });
 
-test('setExtension › Removes additional values', t => {
+test('setExtension › Removes additional values', (t) => {
 	const item = new Item('My wall is painted the color:blue color:yellow @home for +housePainting');
-	item.setExtension('color', 'red')
-	t.deepEqual(item.extensions(), [{key: 'color', value: 'red'}]);
-	t.is(item.body(), 'My wall is painted the color:red @home for +housePainting')
+	item.setExtension('color', 'red');
+	t.deepEqual(item.extensions(), [{ key: 'color', value: 'red' }]);
+	t.is(item.body(), 'My wall is painted the color:red @home for +housePainting');
 });
 
-test('addExtension › Allows for multiple of the same key', t => {
+test('addExtension › Allows for multiple of the same key', (t) => {
 	const item = new Item('My wall is painted the color:blue');
-	item.addExtension('color', 'red')
-	t.deepEqual(item.extensions(), [{key: 'color', value:'blue'}, {key: 'color', value: 'red'}]);
-	t.is(item.body(), 'My wall is painted the color:blue color:red')
+	item.addExtension('color', 'red');
+	t.deepEqual(item.extensions(), [
+		{ key: 'color', value: 'blue' },
+		{ key: 'color', value: 'red' },
+	]);
+	t.is(item.body(), 'My wall is painted the color:blue color:red');
 });
 
-test('removeExtension › Removes the extension by key', t => {
+test('removeExtension › Removes the extension by key', (t) => {
 	const item = new Item('My room:kitchen wall is painted color:blue and color:red');
 	item.removeExtension('color');
-	t.deepEqual(item.extensions(), [{key: 'room', value: 'kitchen'}]);
-	t.is(item.body(), 'My room:kitchen wall is painted and')
+	t.deepEqual(item.extensions(), [{ key: 'room', value: 'kitchen' }]);
+	t.is(item.body(), 'My room:kitchen wall is painted and');
 });
 
-test('removeExtension › Removes the extension by key and value', t => {
+test('removeExtension › Removes the extension by key and value', (t) => {
 	const item = new Item('My room:kitchen wall is painted color:blue and color:red');
 	item.removeExtension('color', 'blue');
-	t.deepEqual(item.extensions(), [{key: 'room', value: 'kitchen'}, {key: 'color', value: 'red'}]);
-	t.is(item.body(), 'My room:kitchen wall is painted and color:red')
+	t.deepEqual(item.extensions(), [
+		{ key: 'room', value: 'kitchen' },
+		{ key: 'color', value: 'red' },
+	]);
+	t.is(item.body(), 'My room:kitchen wall is painted and color:red');
 });
--- src/Item.outputs.test.ts
+++ src/Item.outputs.test.ts
@@ -1,35 +1,45 @@
-import test from 'ava'
-import { Item } from './Item'
+import test from 'ava';
+import { Item } from './Item';
 
-const sampleCompleted = 'x (Z) 2022-10-17 2022-09-03 We should keep +todoItems in their @place when rendering out due:2022-10-22';
+const sampleCompleted =
+	'x (Z) 2022-10-17 2022-09-03 We should keep +todoItems in their @place when rendering out due:2022-10-22';
 
-test('toString › Keeps positioning of tags', t => {
+test('toString › Keeps positioning of tags', (t) => {
 	const item = new Item(sampleCompleted);
 	t.is(item.toString(), sampleCompleted);
 });
 
-test('toString › Appends new tags to the end', t => {
+test('toString › Appends new tags to the end', (t) => {
 	const item = new Item(sampleCompleted);
 	item.addProject('rewrite');
 	item.addContext('computer');
 	item.addExtension('h', '1');
 	t.is(item.toString(), `${sampleCompleted} +rewrite @computer h:1`);
-})
+});
 
-test('toAnnotatedString › Returns the correct string', t => {
+test('toAnnotatedString › Returns the correct string', (t) => {
 	const itemStr = '(B) 2022-01-04 My @wall is +painted the color:blue';
 	const item = new Item(itemStr);
 	const annotated = item.toAnnotatedString();
 	t.is(annotated.string, itemStr);
 });
 
-test('toAnnotatedString › Returns the correct ranges', t => {
+test('toAnnotatedString › Returns the correct ranges', (t) => {
 	const itemStr = '(B) 2022-01-04 My @wall is +painted the color:blue';
 	const item = new Item(itemStr);
 	const annotated = item.toAnnotatedString();
-	t.deepEqual(annotated.contexts.map(ctx => ctx.string), ['@wall']);
-	t.deepEqual(annotated.projects.map(prj => prj.string), ['+painted']);
-	t.deepEqual(annotated.extensions.map(ext => ext.string), ['color:blue']);
+	t.deepEqual(
+		annotated.contexts.map((ctx) => ctx.string),
+		['@wall']
+	);
+	t.deepEqual(
+		annotated.projects.map((prj) => prj.string),
+		['+painted']
+	);
+	t.deepEqual(
+		annotated.extensions.map((ext) => ext.string),
+		['color:blue']
+	);
 	annotated.contexts.forEach((ctx) => {
 		t.is(annotated.string.slice(ctx.span.start, ctx.span.end), ctx.string);
 	});
@@ -39,5 +49,4 @@ test('toAnnotatedString › Returns the correct ranges', t => {
 	annotated.extensions.forEach((ext) => {
 		t.is(annotated.string.slice(ext.span.start, ext.span.end), ext.string);
 	});
-
 });
