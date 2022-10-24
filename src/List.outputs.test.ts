8938d6ebf5f04acb3fa7258bf77a839683d39045
John Hobbs <john@velvetcache.org>
2022-10-24 16:14:35 -0500

    Add projects, contexts and extensions to List

--- src/List.outputs.test.ts
+++ src/List.outputs.test.ts
@@ -5,3 +5,18 @@ test('toString', (t) => {
 	const list = new List(['first item', 'second item', 'third item']);
 	t.is(list.toString(), 'first item\nsecond item\nthird item');
 });
+
+test('projects', (t) => {
+	const list = new List(['first +item', 'second +item', 'third +task']);
+	t.deepEqual(list.projects(), ['item', 'task']);
+});
+
+test('contexts', (t) => {
+	const list = new List(['first @item', 'second @task', 'third @item']);
+	t.deepEqual(list.contexts(), ['item', 'task']);
+});
+
+test('extensions', (t) => {
+	const list = new List(['first item is due:2022-01-05', 'second item h:1', 'third h:0']);
+	t.deepEqual(list.extensions(), ['due', 'h']);
+});
