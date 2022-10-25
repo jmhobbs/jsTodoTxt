58f3bfb3809cb070dd424157da62ca40b70eadfb
John Hobbs <john@velvetcache.org>
2022-10-25 17:08:11 -0500

    Strip blank lines in List constructor

--- src/List.inputs.test.ts
+++ src/List.inputs.test.ts
@@ -6,7 +6,17 @@ test('constructor › string', (t) => {
 	t.is(list.items().length, 2);
 });
 
+test('constructor › string › strips blank lines', (t) => {
+	const list = new List('first item\nsecond item\n\nthird item\n\n');
+	t.is(list.items().length, 3);
+});
+
 test('constructor › array', (t) => {
 	const list = new List(['first item', 'second item', 'third item']);
+	t.is(list.items().length, 3);
+});
+
+test('constructor › array › strips blank lines', (t) => {
+	const list = new List(['first item', '', 'second item', '   ', '\t', 'third item']);
 	t.is(list.items().length, 3);
 });
